d791b32d74ba3d73f088f98741ae5433ee6464ab
Kenza Tazi <ktazi100@yahoo.co.uk>
2022-06-03 07:21:36 +0100

    new metrics

--- experiments/spatiotemporal_stationary.py
+++ experiments/spatiotemporal_stationary.py
@@ -14,11 +14,12 @@ from gpytorch.kernels import RBFKernel, ScaleKernel, PeriodicKernel, MaternKerne
 from sklearn.utils import shuffle
 import scipy.stats
 from scipy.special import inv_boxcox
+from utils.metrics import nlpd, rmse
 
 filepath = 'data/uib_2000_2010_tp.csv'
 
 # Kernel
-kernel = gpytorch.kernels.ScaleKernel(RBFKernel(ard_num_dims=2, active_dims=[1,2])+ gpytorch.kernels.RBFKernel(ard_num_dims=1, active_dims=[0]) * gpytorch.kernels.PeriodicKernel(active_dims=[0]))
+kernel = gpytorch.kernels.ScaleKernel(RBFKernel(ard_num_dims=2, active_dims=[1,2])+ gpytorch.kernels.RBFKernel(active_dims=[0]) * gpytorch.kernels.PeriodicKernel(active_dims=[0]))
 
 df = pd.read_csv(filepath)
 #data_df = shuffle(df, random_state=random_state)
@@ -28,8 +29,11 @@ X = data[:394*5,:-1]
 X = X - X.min(0)[0]
 X = 2 * (X / X.max(0)[0]) - 1
 y = data[:394*5, -1]
-y_tr, bc_param = scipy.stats.boxcox(y + 0.001)
+y_tr, bc_param = scipy.stats.boxcox(y)
 y_tr = torch.Tensor(y_tr)
+
+stdy_tr, _ = torch.std_mean(y_tr)
+stdy, _ = torch.std_mean(y_tr)
 
 train_n = 394*4
 train_x = X[:train_n, :].contiguous()
@@ -51,7 +55,7 @@ model = m.ExactGPModel(train_x, train_y, likelihood, kernel)
 #model.covar_module.outputscale = outputscale_prior.mean
 
 #### Training
-training_iter = 50
+training_iter = 200
 
 # Find optimal model hyperparameters
 model.train()
@@ -74,34 +78,29 @@ for i in range(training_iter):
     # Calc loss and backprop gradients
     loss = -mll(output, train_y)
     loss.backward()
-    #print('Iter %d/%d - Loss: %.3f'  # lengthscale: %.3f   noise: %.3f' 
-    #    % (i + 1, training_iter, loss.item(),
+    print('Iter %d/%d - Loss: %.3f'  # lengthscale: %.3f   noise: %.3f' 
+        % (i + 1, training_iter, loss.item()))
         #model.covar_module.base_kernel.lengthscale.item(),
         #model.likelihood.noise.item()))
     optimizer.step()
 
 
 #### Metrics
-def negative_log_predictive_density(test_y, predicted_mean, predicted_var): 
-    # Vector of log-predictive density per test point    
-    lpd = torch.distributions.Normal(predicted_mean, torch.sqrt(predicted_var)).log_prob(test_y)
-    # return the average
-    return -torch.mean(lpd)
-
-def sqrt_mean_squared_error(test_y, predicted_mean):  
-    return torch.sqrt(torch.mean((test_y - predicted_mean)**2))
 
 model.eval()
 with torch.no_grad():
-    trained_pred_dist = likelihood(model(test_x))
-    predictive_mean = trained_pred_dist.mean
-    predictive_variances = trained_pred_dist.variance
+    pred_y_test = likelihood(model(test_x)) 
+    y_mean = pred_y_test.loc.detach()
+    y_var = pred_y_test.covariance_matrix.diag().sqrt().detach()
 
 # Inverse transform predictions
-predictive_means_tr = torch.Tensor(inv_boxcox(predictive_mean, bc_param))
-vanilla_variances = torch.Tensor(np.ones((len(predictive_variances))))
+# pred_y_test_tr = torch.Tensor(inv_boxcox(pred_y_test, bc_param))
+y_mean_tr = torch.Tensor(inv_boxcox(y_mean, bc_param))
+y_var_tr = torch.Tensor(inv_boxcox(y_var + y_mean, bc_param,)) - y_mean_tr
+test_y_tr = torch.Tensor(inv_boxcox(test_y, bc_param))
 
-rmse = sqrt_mean_squared_error(test_y, predictive_means_tr)
-nlpd = negative_log_predictive_density(test_y, predictive_means_tr, vanilla_variances)
+## Metrics
+rmse_test = rmse(y_mean_tr, test_y_tr, stdy)
+nlpd_test = nlpd(pred_y_test, test_y, stdy_tr)
 
-print(f"RMSE: {rmse.item()}, NLPD: {nlpd.item()}")
+print(f"RMSE: {rmse_test.item()}, NLPD: {nlpd_test.item()}")
